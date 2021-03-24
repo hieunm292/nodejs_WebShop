a2e616e0ef4aff836cd122dd7666d90e9d04b390
= <=>
2021-03-24 11:39:42 +0700

    deletedCount

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,10 +4,22 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 class MeController{
 
     //GET/me/stored/courses
-    storedCourses(req, res,next){ 
-        Course.find({})
-        .then(courses=>res.render('me/stored-courses',{courses:multipleMongooseToObject(courses)}))
-        .catch(next)
+    // storedCourses(req, res,next){ 
+    //     Course.find({})
+    //     .then(courses=>res.render('me/stored-courses',{courses:multipleMongooseToObject(courses)}))
+    //     .catch(next)
+    // }
+
+    // [GET] /me/stored/courses
+    storedCourses(req, res, next) {
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) =>
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongooseToObject(courses),
+                }),
+            )
+            .catch(next);
     }
 
     //GET/me/trash/courses
