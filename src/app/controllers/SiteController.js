d45f84e7c721412b38e915f6af05a8be161a9ac9
= <=>
2021-04-15 20:49:40 +0700

    mm

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -5,12 +5,6 @@ class SiteController{
 
     //GET/
     index(req, res,next){ 
-        // Course.find({}, function (err, courses) {
-        //     if(!err) {
-        //         res.json(courses);
-        //     } else{
-        //     res.status(400).json({error : 'ERROR !!!'});}
-        //   });
 
         Course.find({})
             .then(courses => {
